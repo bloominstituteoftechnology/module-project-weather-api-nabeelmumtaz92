1b033d19fef8731e9ee53d500a7b395e499abb7a
nabeelmumtaz92 <153012098+nabeelmumtaz92@users.noreply.github.com>
2024-01-27 07:29:36 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,84 @@
+const { default: axios } = require("axios")
+
+async function moduleProject4() {
+
+  // 👇 WORK WORK BELOW THIS LINE 👇
+  const footer = document.querySelector('footer')
+  const currentYear = new Date().getFullYear()
+  footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
+
+  let descriptions = [
+    ["Sunny", "☀️"],
+    ["Cloudy", "☁️"],
+    ["Rainy", "🌧️"],
+    ["Thunderstorm", "⛈️"],
+    ["Snowy", "❄️"],
+    ["Partly Cloudy", "⛅️"]
+  ]
+
+  // 👉 Tasks 1 - 5 go here
+document.querySelector('#weatherWidget').style.display = 'none'
+document.querySelector('#citySelect').addEventListener('change', async evt => {
+  try {
+    evt.target.setAttribute('disabled', 'disabled')
+    document.querySelector('#weatherWidget').style.display = 'none'
+    document.querySelector('.info').textContent = 'Fetching weather data...'
+  
+  let city = evt.target.value
+  let url = `http://localhost:3003/api/weather?city=${city}`
+  
+const res = await axios.get(url)
+
+document.querySelector('#weatherWidget').style.display = 'block'
+document.querySelector ('.info').textContent = ''
+evt.target.removeAttribute('disabled')
+
+let (data) = res
+
+document.querySelector('#apparentTemp div:nth-child(2)')
+    .textContent = `${data.current.apparent_temperature}*`
+document.querySelector('#todayDescription')
+    .textContent = descriptions.find(d => d[0]=== data.current.weather_descrption)[1]
+document.querySelector('#todayStats div:nth-child(1)')
+    .textContent = `${data.current.temperature_min}/ ${data.current.temperatur_max}`
+document.querySelector('#todayStats div:nth-child(2)')
+    .textContent = `Precipitation: ${data.current.precipitation_probability * 100}%`
+document.querySelector('#todayStats div:nth-chidl (3)')
+    .textContent = `Humidity: ${data.current.humidity}%`
+document.querySelector ('#todayStats div:nth-child (4)')
+    .textContent = `Wind: ${data.currnt.wind_speed} m/s`
+
+    data.forecast.daily.forEach((data,idx) => {
+      let card = document.querySelectorAll('.next-day')[idx]
+
+      let weekDay = card.children[0]
+      let apparent = card.children [1]
+      let minMax = card.children[2]
+      let precipit = card.children[3]
+
+      weekDay.textcontent = getWeekDay(day.date)
+      apparent.textContent = descriptions.find(d=> d[0] === day.weather_description)[1]
+      minMax.textContent = `${day.temperature_min}/${day.temperature_max}`
+      precipit.textContent = `Precipitation: ${day.precipitation_probability *100}%`
+    })
+
+  document.querySelector('#location').firstElementChild.textContent = data.location.city
+
+
+  } catch (err) {
+
+  }
+})
+
+function getWeekDay(date) {
+  return data
+}
+  // 👆 WORK WORK ABOVE THIS LINE 👆
+
+}
+
+// ❗ DO NOT CHANGE THE CODE  BELOW
+// ❗ DO NOT CHANGE THE CODE  BELOW
+// ❗ DO NOT CHANGE THE CODE  BELOW
+if (typeof module !== 'undefined' && module.exports) module.exports = { moduleProject4 }
+else moduleProject4()
